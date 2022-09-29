36ae70ab6cb9a5c4e3c767d50034e15ece09b5d5
sooyoungh <sooyoung.h8@gmail.com>
2022-09-30 00:36:04 +0900

    Repository 수정 및 추가

--- src/main/java/com/example/murange/Repository/LikeRepository.java
+++ src/main/java/com/example/murange/Repository/LikeRepository.java
@@ -1,9 +1,15 @@
 package com.example.murange.Repository;
 
 import com.example.murange.Domain.Like;
+import com.example.murange.Domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long>{
+
+    List<Like> findAllByUser(User user);
+
 }
--- src/main/java/com/example/murange/Repository/MusicRepository.java
+++ src/main/java/com/example/murange/Repository/MusicRepository.java
@@ -4,7 +4,11 @@ import com.example.murange.Domain.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MusicRepository extends JpaRepository<Music, Long> {
+
+    List<Music> findAllByOrderByCntDesc();
 
 }
