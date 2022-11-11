2d92f619497bc604236bedba180de6c12f13db18
sanho <nalgae415@naver.com>
2022-11-12 02:16:53 +0900

    feat : 메인페이지 페이지네이션

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,65 +1,51 @@
-var user_id = '0'
-var random = "";
 
-$(document).ready(function () {
-    const promise1 = new Promise((resolve, reject) => {
-            $.ajax({
-                url: "http://localhost:8080/random/title",
-                data: 'get',
-                contentType: "application/json;charset=UTF-8",
-                dataType: "text",
-                success: function (data) {
-                    var random_title = data.charAt(0).toUpperCase() + data.slice(1);
-                    document.getElementsByClassName('topic')[0].innerText = random_title;
-                    random = data;
-                    resolve();
-                },
-                error: function () {
-                    console.log('error')
-                }
-            });
+let container = $('#pagination');
+var tmp;
+$(function () {
+    const promise_random = new Promise((resolve, reject) => {
+        $.ajax({
+            url: "http://localhost:8080/random/title",
+            data: 'get',
+            contentType: "application/json;charset=UTF-8",
+            dataType: "text",
+            success: function (data) {
+                var random_title = data.charAt(0).toUpperCase() + data.slice(1);
+                document.getElementsByClassName('topic')[0].innerText = random_title;
+                random = data;
+                resolve();
+            }
         });
+    });
+    promise_random.then(()=> {
+        $.ajax({
+            url: "http://localhost:8080/random/" + random,
+            data: 'get',
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function (dataList) {
+                tmp = dataList.content;
+                container.pagination({
+                    dataSource: tmp,
+                    pageSize:5,
+                    // showPrevious: false,
+                    // showNext: false,
+                    callback: function (data, pagination) {
+                        $(tmp).each(function (index, data) {
+                        });
+                        var dataHtml = '<ul>';
+                        $.each(data, function (index, item) {
+                            dataHtml += '<li class="playlist"> <a href="'+item.soundcloud_url+'"><img class="album" src="'+item.img_url+'"><p class="title">'+item.title+'</p></a>'+ '</li>';
+                        });
 
-        promise1.then(()=>{
-            $.ajax({
-                url: "http://localhost:8080/random/" + random,
-                data: 'get',
-                contentType: "application/json;charset=UTF-8",
-                dataType: "json",
-                success: function (dataList) {
-                console.log(dataList);
-                var tmp = dataList.content;
-                console.log(tmp);
-                    $(tmp).each(function (index, data) {
-                        console.log(data.img_url);
-                        console.log(data.title);
-                        console.log(data.soundcloud_url);
-                        $(".sample")
-                            .append(
-                                $("<li>")
-                                .addClass("playlist")
-                                .append(
+                        dataHtml += '</ul>';
 
-                                    $("<a>")
-                                    .attr({href:data.soundcloud_url})
-                                    .append(
-                                        $("<img>")
-                                            .addClass("album")
-                                            .attr({
-                                                src : data.img_url
-                                            }),
-                                        $("<p>")
-                                            .addClass("title")
-                                            .text(data.title)
-                                    )
-                                )
-                            )
-                    });
-                },
-                error: function () {
-                    console.log("메인 페이지 음악 정보 로딩 에러");
-                }
-            });
+                        $("#data-container").html(dataHtml);
+                    }
+                })
+            },
+            error: function () {
+            }
+        });
+    });
 
-        })
-});
+})
