7a1ca7cc42669ff8f9e4cd42a174e40570b977e0
sanho <nalgae415@naver.com>
2022-11-28 02:15:00 +0900

    feat : 메인 페이지 로딩 기능, 애니메이션

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,6 +2,34 @@
 let container = $('#pagination');
 var tmp ;
 var ul = new Array();
+
+$(document).ready(function openLoading() {
+    //화면 높이와 너비를 구합니다.
+    let maskHeight = $(document).height();
+    let maskWidth = window.document.body.clientWidth;
+    //출력할 마스크를 설정해준다.
+    let mask ="<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
+    // 로딩 이미지 주소 및 옵션
+    let loadingImg ='';
+    loadingImg += "<div id='loadingImg' style='position:absolute; top: calc(50% - (200px / 2)); width:100%; z-index:99999999;'>";
+    loadingImg += " <img src='https://loadingapng.com/animation.php?image=4&fore_color=000000&back_color=FFFFFF&size=128x128&transparency=1&image_type=0&uncacher=75.5975991029623' style='position: relative; display: block; margin: 0px auto;'/>";
+    loadingImg += "</div>";
+    //레이어 추가
+    $('body')
+        .append(mask)
+        .append(loadingImg)
+    //마스크의 높이와 너비로 전체 화면을 채운다.
+    $('#mask').css({
+        'width' : maskWidth,
+        'height': maskHeight,
+        'opacity' :'0.3'
+    });
+    //마스크 표시
+    $('#mask').show();
+    //로딩 이미지 표시
+    $('#loadingImg').show();
+});
+
 $(function () {
     $.ajax({
         url: "http://localhost:8080/user",
@@ -31,6 +59,11 @@ $(function () {
         });
 
     });
+    // 로딩창 끄는 함수
+    function closeLoading() {
+        $('#mask, #loadingImg').hide();
+        $('#mask, #loadingImg').empty();
+    }
     promise_random.then(()=> {
             for (i = 0; i < 3; i++) {
                 $.ajax({
@@ -57,8 +90,6 @@ $(function () {
                 showPrevious: false,
                 showNext: false,
                 callback: function (data, pagination) {
-                    $(ul).each(function (index, data) {
-                    });
                     var dataHtml = '<ul>';
                     $.each(data, function (index, item) {
                         dataHtml += '<li class="playlist"> <a href="' + item.soundcloud_url + '" target="_blank"><img class="album" src="' + item.img_url + '"><p class="title">' + item.title + '</p></a>' + '</li>';
@@ -67,6 +98,8 @@ $(function () {
                     dataHtml += '</ul>';
 
                     $("#data-container").html(dataHtml);
+                    closeLoading()
+                    $(".playlist__emotion").css('display','block');
                 }
             })
     });
